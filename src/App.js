5fba9665d2057a01ecc195ac18fbf954ad276c99
BrianArchibald <brianallenarchibald@gmail.com>
2019-01-15 21:08:11 -0600

    added react react dom and parcel

--- src/App.js
+++ src/App.js
@@ -1,33 +1,55 @@
-const Pet = (props) => {
-	return React.createElement('div', {}, [
-		React.createElement('h1', {}, props.name),
-		React.createElement('h1', {}, props.animal),
-		React.createElement('h1', {}, props.breed)
-	]);
-}
+import React from "react";
+import { render } from 'react-dom';
+import pf from 'petfinder-client';
+
+import Pet from "./Pet";
+
+const petfinder = pf({
+	key: process.env.API_KEY,
+	secret: process.env.API_SECRET
+});
+
 
 class App extends React.Component {
+	componentDidMount() {
+		const promise = petfinder.breed.list({ animal: 'dog' })
+
+		promise.then(console.log, console.error);
+	}
+
 	render() {
-		return React.createElement('div', {}, [
-		React.createElement('h1', {}, 'Adopt Me!'),
-		React.createElement(Pet, {
-			name: 'Luna',
-			animal: 'dog',
-			breed: 'Havenese'
-		}),
-		React.createElement(Pet, {
-			name: 'Pepper',
-			animal: 'bird',
-			breed: 'Cockatiel'
-		}),
-		React.createElement(Pet, {
-			name: 'Doink',
-			animal: 'cat',
-			breed: 'Mixed'
-		})
-		])
+		// return React.createElement('div', {}, [
+		// 	React.createElement(
+		// 		'h1', 
+		// 		{ onClick: this.handleTitleClick }, 
+		// 		'Adopt Me!'
+		// 		),
+		// 	React.createElement(Pet, {
+		// 		name: 'Luna',
+		// 		animal: 'dog',
+		// 		breed: 'Havenese'
+		// 	}),
+		// 	React.createElement(Pet, {
+		// 		name: 'Pepper',
+		// 		animal: 'bird',
+		// 		breed: 'Cockatiel'
+		// 	}),
+		// 	React.createElement(Pet, {
+		// 		name: 'Doink',
+		// 		animal: 'cat',
+		// 		breed: 'Mixed'
+		// 	})
+		// ])
+
+		return(
+			<div>
+				<h1>Adopt Me</h1>
+				<Pet name="Luna" animal="dog" breed="Havenese" />
+				<Pet name="Pepper" animal="bird" breed="Cockatiel" />
+				<Pet name="Doink" animal="cat" breed="Mixed" />
+			</div>
+		);
 	}
 }
 
-
-ReactDOM.render(React.createElement(App), document.getElementById('root'))
+render(<App />, document.getElementById('root'))
