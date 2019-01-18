c86f553d07e611098cf3019266a4fcce8090a080
BrianArchibald <brianallenarchibald@gmail.com>
2019-01-17 20:12:43 -0600

    Added detail component and results comp

--- src/Pet.js
+++ src/Pet.js
@@ -1,27 +1,30 @@
 import React from "react";
 
 class Pet extends React.Component {
-	render() {
-		const { name, animal, breed, media, location } = this.props;
+  render() {
+    const { name, animal, breed, media, location } = this.props;
+    let photos = [];
+    if (media && media.photos && media.photos.photo) {
+      photos = media.photos.photo.filter(photo => photo["@size"] === "pn");
+    }
 
-		let photos = [];
+    let hero = "http://placecorgi.com/300/300";
+    if (photos[0] && photos[0].value) {
+      hero = photos[0].value;
+    }
 
-		if (media && media.photos && media.photos.photo) {
-			photos = media.photos.photo.filter(photo => photo['@size'] === 'pn')
-		}
-
-		return (
-			<div className="pet">
-				<div className="image-container">
-					<img src={photos[0].value} alt={name} />
-				</div>
-				<div className="info">
-					<h1>{name}</h1>
-					<h2>{animal} - {breed} - {location}</h2>
-				</div>
-			</div>
-		);
-	}
+    return (
+      <div className="pet">
+        <div className="image-container">
+          <img src={hero} alt={name} />
+        </div>
+        <div className="info">
+          <h1>{name}</h1>
+          <h2>{`${animal} — ${breed} — ${location}`}</h2>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Pet;
